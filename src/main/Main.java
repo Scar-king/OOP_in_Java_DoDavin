f468fc9b150cee1660fe4aaf40155f0126ef3d46
vath <bro.vath7547@gmail.com>
2025-06-15 21:13:27 +0700

    Push testing

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,16 +5,20 @@ import javax.swing.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		JFrame window = new JFrame("Game");
-		
-		
-		window.setResizable(false);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		window.setVisible(true);
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Game");
+        window.setLayout(new BorderLayout());
 
-	}
+        JLabel label = new JLabel("Hello World", SwingConstants.CENTER);
+        label.setFont(new Font("Serif", Font.BOLD, 64)); 
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
 
+        window.add(label, BorderLayout.CENTER);
+
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        window.setVisible(true);
+    }
 }
